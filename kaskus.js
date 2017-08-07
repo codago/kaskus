da86540a9ccfe7a5a31372e2873663cff1f82fd7
chrisantoni <tonichen184@gmail.com>
2017-08-07 14:16:11 +0700

    commit-2

--- kaskus.js
+++ kaskus.js
@@ -1,16 +1,34 @@
+// function deretKaskus(n){
+//   //write code here
+//   for(var i=3;i<=n;i+=3){
+//     if(i % 5 === 0 && i% 6 === 0){
+//         console.log("KASKUS");
+//     }else if(i % 5 === 0){
+//         console.log("KAS");
+//     }else if(i%6 ===0){
+//         console.log("KUS");
+//     }else{
+//         console.log(i);
+//     }
+//   }
+// }
+
+
 function deretKaskus(n){
-  //write code here
-  for(var i=3;i<=n;i+=3){
-    if(i % 5 === 0 && i% 6 === 0){
+    var count = 3;
+    do{
+        if(count % 5 === 0 && count% 6 === 0){
         console.log("KASKUS");
-    }else if(i % 5 === 0){
+    }else if(count % 5 === 0){
         console.log("KAS");
-    }else if(i%6 ===0){
+    }else if(count%6 ===0){
         console.log("KUS");
     }else{
-        console.log(i);
+        console.log(count);
     }
-  }
+        count+=3;
+    }while(count<=n);
 }
+
 
 deretKaskus(30);
