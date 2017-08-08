ee3e873cda477d061d4ee271ee3a133c5cc715d7
fahmi <fahmighifari72@gmail.com>
2017-08-08 16:42:27 +0700

    fahmi

--- kaskus.js
+++ kaskus.js
@@ -1,16 +1,19 @@
 function deretKaskus(n){
 
-var deret = [];
-var kelipatan = 0;
-    for( var i = 3; i <= n; i+=3){
-    if (i %5 == 0 && i %6 == 0) {console.log("kaskus")}
-    else if (i %5 == 0){
-    console.log ("kas")
-    }else if (i %6 == 0){
-    console.log ("kus")}
-    else {console.log (i)}
-
+  var deret = [];
+  var multi = 0;
+  for( var i = 0; i<n; i++){
+    multi += 3;
+    if ((multi % 5) === 0 && (multi % 6) === 0) {
+      deret.push("kaskus")
+    } else if (multi % 5 === 0){
+      deret.push("kas")
+    } else if (multi % 6 === 0){
+      deret.push("kus")
+    } else {
+      deret.push(multi)
+    }
+  }
+  return deret;
 }
-}
-
-deretKaskus(15)
+console.log(deretKaskus(10));
